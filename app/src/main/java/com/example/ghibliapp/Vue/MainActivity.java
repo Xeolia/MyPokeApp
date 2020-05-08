e3d7232ed1160d3a8acec48c5a6ea0b889e27daa
Xeolia <alexia.serrierb@gmail.com>
2020-05-08 15:30:24 +0200

    Ajout des images à chaque vue

--- app/src/main/java/com/example/ghibliapp/Vue/MainActivity.java
+++ app/src/main/java/com/example/ghibliapp/Vue/MainActivity.java
@@ -74,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+
         // define an adapter
         mAdapter = new ListAdapter(ghibliList);
         recyclerView.setAdapter(mAdapter);
--- app/src/main/java/com/example/ghibliapp/Vue/MySecondFragment.java
+++ app/src/main/java/com/example/ghibliapp/Vue/MySecondFragment.java
@@ -1,10 +1,13 @@
 package com.example.ghibliapp.Vue;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +25,6 @@ public class MySecondFragment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_fragment);
         Button one = (Button) findViewById(R.id.buttonRetour);
-        // String sent=intent.getStringExtra("Decription");
 
         TextView monTitre = (TextView) findViewById(R.id.monTitre);
         valueFilm = "Titre :" + getIntent().getStringExtra("Titre");
@@ -33,15 +35,18 @@ public class MySecondFragment extends AppCompatActivity {
         maDecription.setText(valueDescription);
 
         TextView monRealisateur = (TextView) findViewById(R.id.monRealisateur);
-        valueRealisateur = "FilmDirector :" + getIntent().getStringExtra("FilmDirector");
+        valueRealisateur = "Film director :" + getIntent().getStringExtra("FilmDirector");
         monRealisateur.setText(valueRealisateur);
 
         TextView maDate = (TextView) findViewById(R.id.maDate);
         valueDate = "Date :" + getIntent().getStringExtra("Date");
         maDate.setText(valueDate);
 
-        //int id = getResources().getIdentifier(mo, "drawable", DetailActivity.this.getPackageName());
-        //flag.setImageResource(id);
+        ImageView image = (ImageView) findViewById(R.id.monImage);
+        int id = getResources().getIdentifier(valueFilm.substring(7,valueFilm.length()).replace(" ", "_").replace("'","").toLowerCase() + "_poster" , "drawable", MySecondFragment.this.getPackageName());
+        image.setImageResource(id);
+
+
 
         one.setOnClickListener(new View.OnClickListener() {
 
