19032506bab992d7dfc5a1f20d1b79b3275d033a
Xeolia <alexia.serrierb@gmail.com>
2020-05-19 15:12:16 +0200

    Ajout fonctionnel du gif dans l'écran de chargement

--- app/src/main/java/com/example/ghibliapp/presentation/Vue/CustomProgressBarActivity.java
+++ app/src/main/java/com/example/ghibliapp/presentation/Vue/CustomProgressBarActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.Glide;
 import com.example.ghibliapp.R;
+
+import java.net.URL;
 
 public class CustomProgressBarActivity extends AppCompatActivity {
 
@@ -19,15 +26,20 @@ public class CustomProgressBarActivity extends AppCompatActivity {
     private int pStatus = 0;
     private Handler handler = new Handler();
     Thread loading;
+    private ImageView monGif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_progressbar);
+        ImageView monGif = (ImageView) findViewById(R.id.imageView);
+        Glide GlideApp;
+        String url = "https://66.media.tumblr.com/tumblr_lpsdjmuPoX1qf94kbo1_500.gifv";
 
-        //txtProgress = (TextView) findViewById(R.id.txtProgress);
-       // progressBar = (ProgressBar) findViewById(R.id.progressBar);
-       // progressBar.setVisibility(View.VISIBLE);
+        Glide.with(getApplicationContext())
+                .load(url)
+                .into(monGif);
+        loadImageByInternetUrl();
 
         loading = new Thread(new Runnable() {
             @Override
@@ -47,4 +59,11 @@ public class CustomProgressBarActivity extends AppCompatActivity {
         loading.start();
     }
 
+
+    private void loadImageByInternetUrl() {
+        //String internetUrl =  ;
+
+
+
+    }
 }
